2fc2df93eca5fdac73d3013df3720abb3146aadc
Velmurugan Velayutham <velmuruganvelayutham@users.noreply.github.com>
2017-04-20 11:57:31 +0530

    Update ubuntu-dev.sh

--- ubuntu-dev.sh
+++ ubuntu-dev.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-echo "------ eclipse installation ------"
+echo "------ eclipse installation begin ------"
 if [ ! -f eclipse.tar.gz ]; then
     wget --tries=2 -O eclipse.tar.gz http://ftp.yz.yamagata-u.ac.jp/pub/eclipse/technology/epp/downloads/release/neon/3/eclipse-jee-neon-3-linux-gtk-x86_64.tar.gz
 fi
 mkdir tools
 tar -zxvf eclipse.tar.gz -C tools/
+sudo ln -s ~/tools/eclipse/eclipse /usr/local/bin/eclipse
+echo "------ eclipse installation done ! ------\n"
 
+echo "------ java installation begin ------"
+echo "------ java installation done ! ------"
 
+echo "------ maven installation begin ------"
+echo "------ maven installation done ! ------"
 
+echo "------ git installation begin ------"
+echo "------ git installation done ! ------"
+
+echo "------ docker installation begin ------"
+echo "------ docker installation done ! ------"
+
+echo "------ mysql installation begin ------"
+echo "------ mysql installation done ! ------"
 
