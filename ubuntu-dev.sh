7df866679a552da51abc265b308933666eacb670
Velmurugan Velayutham <velmuruganvelayutham@users.noreply.github.com>
2017-04-20 10:50:41 +0530

    Update ubuntu-dev.sh

--- ubuntu-dev.sh
+++ ubuntu-dev.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 echo "------ eclipse installation ------"
 wget --tries=2 -O eclipse.tar.gz http://ftp.yz.yamagata-u.ac.jp/pub/eclipse/technology/epp/downloads/release/neon/3/eclipse-jee-neon-3-linux-gtk-x86_64.tar.gz
