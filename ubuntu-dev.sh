d161c7196b2e9ed428e03de558657bd3974a408b
Velmurugan Velayutham <velmuruganvelayutham@users.noreply.github.com>
2017-04-21 07:11:30 +0530

    Update ubuntu-dev.sh

--- ubuntu-dev.sh
+++ ubuntu-dev.sh
@@ -4,7 +4,7 @@ if [ ! -f eclipse.tar.gz ]; then
     wget --tries=2 -O eclipse.tar.gz http://ftp.yz.yamagata-u.ac.jp/pub/eclipse/technology/epp/downloads/release/neon/3/eclipse-jee-neon-3-linux-gtk-x86_64.tar.gz
 fi
 mkdir tools
-tar -zxvf eclipse.tar.gz -C ~/tools/
+tar -zxf eclipse.tar.gz -C ~/tools/
 ln -s ~/tools/eclipse/eclipse /usr/local/bin/eclipse
 echo "------ eclipse installation done ! ------\n"
 
@@ -12,7 +12,7 @@ echo "------ java installation begin ------"
 if [ ! -f java.tar.gz ]; then
 wget -O java.tar.gz --tries=2 --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jdk-8u112-linux-x64.tar.gz
 fi
-tar -zxvf java.tar.gz -C ~/tools/
+tar -zxf java.tar.gz -C ~/tools/
 sudo update-alternatives --install /usr/local/bin/java java ~/tools/jdk1.8.0_112/bin/java 100
 sudo update-alternatives --install /usr/local/bin/javac javac ~/tools/jdk1.8.0_112/bin/javac 100
 java -version && echo "OK Java"
@@ -23,7 +23,7 @@ echo "------ maven installation begin ------"
 if [ ! -f maven.tar.gz ]; then
   wget -O maven.tar.gz --tries=2 http://apache.communilink.net/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
 fi
-tar xzvf maven.tar.gz -C ~/tools/
+tar xzf maven.tar.gz -C ~/tools/
 sudo update-alternatives --install /usr/local/bin/mvn mvn ~/tools/apache-maven-3.5.0/bin/mvn 100
 mvn --version && echo "OK Maven"
 echo "------ maven installation done ! ------"
