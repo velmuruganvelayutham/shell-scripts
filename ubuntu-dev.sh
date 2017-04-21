d6400afad616c04f496f973421c536e93f474691
Velmurugan Velayutham <velmuruganvelayutham@users.noreply.github.com>
2017-04-21 16:49:09 +0530

    Update ubuntu-dev.sh

--- ubuntu-dev.sh
+++ ubuntu-dev.sh
@@ -51,12 +51,10 @@ if [ ! -f /usr/bin/docker ]; then
       sudo apt-get -y install docker-ce
       sudo groupadd docker
       sudo usermod -aG docker $USER
-      curl -L --fail "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-      chmod +x /usr/local/bin/docker-compose
+      sudo curl -L --fail "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+      sudo chmod +x /usr/local/bin/docker-compose
 
 fi
 echo "------ docker installation done ! ------"
 
-echo "------ mysql installation begin ------"
-echo "------ mysql installation done ! ------"
 
